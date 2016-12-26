5931cffe91526bfcd23ee085d090820d50ccc531
Amit Znati <amitznati@gmail.com>
2016-12-26 23:06:53 +0200

    Revert "Revert "Part 43 - Adding a WYSIWYG Editor + post edit layout""
    This reverts commit 6d6d992c128afd315e47df306865a9f324ba1499.

---
+++ resources/views/partials/_post_edit.blade.php
@@ -0,0 +1,14 @@
+{{ Form::label('title','Title:') }}
+{{ Form::text('title',null,array('class' => 'form-control', 'required' => '',  'placeholder' => 'Title')) }}
+<br>
+{{ Form::label('slug', 'Slug:') }}
+{{ Form::text('slug', null, array('class'=>'form-control', 'required'=>'','minlength' => '5', 'maxlength' => '255' )) }}
+<br>
+{{ Form::label('category_id','Category:') }}
+{{ Form::select('category_id',$categories, null, ["class" => "form-control"]) }}
+<br>
+{{ Form::label('tags','Tags:') }}
+{{ Form::select('tags[]',$tags, null, ["class" => "form-control select2-multi", 'multiple' => 'multiple']) }}
+<br>
+{{ Form::label('body','Post Body:') }}
+{{ Form::textarea('body',null ,array('class' => 'form-control', 'required' => '', 'placeholder' => 'Free Text Here...')) }}
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,74 +1,46 @@
-@extends('layouts.main')
+@extends('layouts.edit_post')
 
 @section('title', '| Edit Post')
 
-@section('stylesheets')
-
-    {!! Html::style('css/parsley.css') !!}
-    {!! Html::style('css/select2.min.css') !!}
-
-@endsection
-
-@section('content')
 
 <div class="row">
-    {!! Form::model($post, ['route' => ['posts.update', $post->id] , 'method' => 'PUT']) !!}
-    <div class="col-md-8">
-        {{ Form::label('title' , 'Title:') }}
-        {{ Form::text('title', null, ["class" => 'form-control']) }}
-        <br>
-        {{ Form::label('slug' , 'Slug:') }}
-        {{ Form::text('slug',null, ["class" => 'form-control']) }}
-        <br>
-        {{ Form::label('category_id','Category:') }}
-        {{ Form::select('category_id',$categories, null, ["class" => "form-control"]) }}
-        <br>
-        {{ Form::label('tags','Tags:') }}
-        {{ Form::select('tags[]',$tags, null, ["class" => "form-control select2-multi", 'multiple' => 'multiple']) }}
-        <br>
-        {{ Form::label('body' , 'Body:') }}
-        {{ Form::textarea('body',null, ["class" => 'form-control']) }}
+    @section('post_form')
+        {!! Form::model($post, ['route' => ['posts.update', $post->id] , 'method' => 'PUT']) !!}
         
-    </div>
-    
-    <div class="col-md-4">
-        <div class="well">
-            <div class="dl-horizontal">
-                <label>Created At:</label>
-                <p>{{ date('M j, Y H:i',strtotime($post->created_at)) }}</p>
-            </div>
-                
-            <div class="dl-horizontal">
-                <label>Last Update:</label>
-                <p>{{ date('M j, Y H:i',strtotime($post->updated_at)) }}</p>
-            </div>
-            <hr>
-            <div class="row">
-                <div class="col-sm-6">
-                    {{ Form::submit('Save Changes',["class" => 'btn btn-success btn-block']) }}
-                    
-                </div>
-                <div class="col-sm-6">
-                    
-                    {!! Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
-                </div>
-            </div>
+        <div class="col-md-8">
+
+            @include('partials._post_edit')
+        
         </div>
-    </div>    
-    <hr>
-    
+        
+
+    @stop
+    @section('post_details')
+        <div class="col-md-4">
+            <div class="well">
+                <div class="dl-horizontal">
+                    <label>Created At:</label>
+                    <p>{{ date('M j, Y H:i',strtotime($post->created_at)) }}</p>
+                </div>
+                    
+                <div class="dl-horizontal">
+                    <label>Last Update:</label>
+                    <p>{{ date('M j, Y H:i',strtotime($post->updated_at)) }}</p>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        {{ Form::submit('Save Changes',["class" => 'btn btn-success btn-block']) }}
+                        
+                    </div>
+                    <div class="col-sm-6">
+                        
+                        {!! Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+                    </div>
+                </div>
+            </div>
+        </div>    
+        <hr>  
+    @endsection 
     {!! Form::close() !!}
 </div>
-
-@endsection
-
-@section('scripts')
-
-    {!! Html::script('js/parsley.min.js') !!}
-    {!! Html::script('js/select2.min.js') !!}
-    <script type="text/javascript">
-        $(".select2-multi").select2();
-        $(".select2-multi").select2().val({!! json_encode($post->tags()->getRelatedIds()) !!}).trigger('change'); 
-    </script>
-
-@endsection
