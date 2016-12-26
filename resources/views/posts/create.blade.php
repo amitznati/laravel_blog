66cd0962ec1d5b1ceda829016864c6f0c63fee12
Amit Znati <amitznati@gmail.com>
2016-12-26 23:07:15 +0200

    part 43 cont.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,60 +1,18 @@
 
-@extends('layouts.main')
+@extends('layouts.edit_post')
 
 @section('title','| Create New Post')
 
-@section('stylesheets')
-
-    {!! Html::style('css/parsley.css') !!}
-    {!! Html::style('css/select2.min.css') !!}
-
-@endsection
-
-@section('content')
-
+@section('post_form')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <h1>Cteate New Post</h1>
             <hr>
-            
             {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
-                {{ Form::label('title','Title:') }}
-                {{ Form::text('title',null,array('class' => 'form-control', 'required' => '',  'placeholder' => 'Title')) }}
-                
-                {{ Form::label('slug', 'Slug:') }}
-                {{ Form::text('slug', null, array('class'=>'form-control', 'required'=>'','minlength' => '5', 'maxlength' => '255' )) }}
-                
-                {{ Form::label('category_id','Category:') }}
-                <select class="form-control" name="category_id">
-                    @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                </select>
-
-                {{ Form::label('tags','Tags:') }}
-                <select class="select2-multi form-control" multiple="multiple" name="tags[]">
-                    @foreach($tags as $tag)
-                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                    @endforeach
-                </select>
-                
-                {{ Form::label('body','Post Body:') }}
-                {{ Form::textarea('body',null ,array('class' => 'form-control', 'required' => '', 'placeholder' => 'Free Text Here...')) }}
-                
+                @include('partials._post_edit')
                 {{ Form::submit('Create Post',array('class' => 'btn btn-success btn-lg btn-block' , 'style' => 'margin-top: 20px') )}}
-            
             {!! Form::close() !!}
         </div>
     </div>
-
-@endsection
-
-@section('scripts')
-
-    {!! Html::script('js/parsley.min.js') !!}
-    {!! Html::script('js/select2.min.js') !!}
-    <script type="text/javascript">
-        $(".select2-multi").select2();
-    </script>
 
 @endsection
