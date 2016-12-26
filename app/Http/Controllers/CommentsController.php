c89d8cffe161fc57c5fb7dd3a411405541305cce
Amit Znati <amitznati@gmail.com>
2016-12-26 21:39:07 +0200

    Comment auth

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -13,24 +13,9 @@ use Session;
 
 class CommentsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $this->middleware('auth',['except' => 'store']);
     }
 
     /**
@@ -63,17 +48,6 @@ class CommentsController extends Controller
 
         return redirect()->route('blog.single', $post->slug);
 
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
     }
 
     /**
